da0bc79c41e95ec9f380217d3a05a360332c07e0
denys_smyk <smukdenys10@gmail.com>
2020-12-10 14:31:04 +0200

    add build

---
+++ 1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "io"
+    "log"
+    "net/http"
+)
+
+func main() {
+	h1 := func(w http.ResponseWriter, _ *http.Request) {
+	    io.WriteString(w, "Hello #1!\n")
+	}
+	http.HandleFunc("/", h1)
+
+	log.Fatal(http.ListenAndServe("127.0.0.1:8093", nil))
+}
+
+//for start
+//go run 1.go
+
+//for build
+//go build 1.go
